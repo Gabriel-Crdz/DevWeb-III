2cc04c8d4726044ae36f7c2dc073698ea54f77c1
Gabriel Cardozo <134400952+Gabriel-Crdz@users.noreply.github.com>
2026-06-02 22:11:23 -0300

    Add files via upload

---
+++ 16-REST/src/main/java/com/example/ordem_servico_api/services/ClienteService.java
@@ -0,0 +1,33 @@
+package com.example.ordem_servico_api.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.ordem_servico_api.entities.Cliente;
+import com.example.ordem_servico_api.repositories.ClienteRepository;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    ClienteRepository clienteRepository;
+    
+    public List<Cliente> findAll(){
+        List<Cliente> clientes = clienteRepository.findAll();
+        return clientes;
+    }
+
+    public Cliente save(Cliente cliente){
+        return clienteRepository.save(cliente);
+    }
+
+    public Cliente update(Cliente cliente, Long id){
+        return clienteRepository.save(cliente); // Usando o mesmo metodo save(), pos o jpa identifica que ja existe um id e sobrescreve a informação
+    }
+
+    public void deleteById(Long id){
+        clienteRepository.deleteById(id); 
+    }
+}
